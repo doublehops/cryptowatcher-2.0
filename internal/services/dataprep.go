916e88f9beaba9c378f800780374574886dfedc0
Damien Buttler <damien@doublehops.com>
2021-10-04 13:54:22 +1100

    Fixed test.

---
+++ internal/services/dataprep.go
@@ -0,0 +1,13 @@
+package services
+
+//import (
+//	"cryptowatcher.example/"
+//)
+//
+//func GetCurrencyTimeSeriesChartData(data model.CmcHistoriesPriceTimeSeriesData) {
+//
+//	var chartData TimeSeriesChartData
+//	for _, v := range data {
+//
+//	}
+//}
---
+++ internal/services/types.go
@@ -0,0 +1,6 @@
+package services
+
+type TimeSeriesChartData struct {
+	Labels []string
+	Dataset []string
+}
