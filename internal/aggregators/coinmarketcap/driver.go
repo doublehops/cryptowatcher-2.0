4fbaf5296b3a677b92ef8d057241b12845ea2006
Damien Buttler <damien@doublehops.com>
2023-02-26 21:28:47 +1100

    Updated method to get config in coinmarketcap aggregator.

--- internal/aggregators/coinmarketcap/driver.go
+++ internal/aggregators/coinmarketcap/driver.go
@@ -38,7 +38,7 @@ type HostConfig struct {
 
 // New will instantiate Runner.
 func New(l *logga.Logga, db dbinterface.QueryAble, client *http.Client) (*Runner, error) {
-	config, err := parseConfig()
+	config, err := parseConfig("./internal/aggregators/coinmarketcap/config.json")
 	if err != nil {
 		// todo - add log message
 		return nil, err
@@ -56,9 +56,8 @@ func New(l *logga.Logga, db dbinterface.QueryAble, client *http.Client) (*Runner
 	}, nil
 }
 
-func parseConfig() (*aggregatorConfig, error) {
+func parseConfig(configFile string) (*aggregatorConfig, error) {
 	var config aggregatorConfig
-	configFile := "internal/aggregators/coinmarketcap/config.json"
 	absPath, _ := filepath.Abs(configFile)
 	f, err := os.ReadFile(absPath)
 	if err != nil {
